43e9ee96328eb5009cd082c0726f0073c567d25f
Michael Dorokhov <dorohov1008@gmail.com>
2026-02-08 14:25:59 +0200

    Add salary calculator

---
+++ pyCore_mod4_task1.py
@@ -0,0 +1,20 @@
+import pathlib
+
+def total_salary(file_path=str()):
+    employee_count = int(0)
+    sal_sum = 0
+    sal_avg = 0
+    path_obj = pathlib.PureWindowsPath(file_path)
+    with open(path_obj.as_posix(), 'r', encoding='UTF-8') as fh:
+        while True:
+            data = fh.readline()
+            if data == '':
+                break
+            sal_sum += int(data.split(',')[1])
+            employee_count += 1
+    sal_avg = sal_sum/employee_count
+    salary_data = (sal_sum, sal_avg)
+    return salary_data
+
+salary_sum, salary_average = total_salary('D:/basic-projects/goit-algo-hw-04/monthly-salary.txt')
+print(f"Загальна сума заробітної плати: {salary_sum}, Середня заробітна плата: {salary_average}")
