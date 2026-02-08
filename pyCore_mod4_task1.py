eaf3492f3132404a6990ec91926844c304d4a9cc
Michael Dorokhov <dorohov1008@gmail.com>
2026-02-08 15:47:17 +0200

    First commit - task2

--- pyCore_mod4_task1.py
+++ pyCore_mod4_task1.py
@@ -1,20 +1,19 @@
-import pathlib
-
 def total_salary(file_path=str()):
-    employee_count = int(0)
+    employee_count = int(0) # count employees for average salary
     sal_sum = 0
     sal_avg = 0
-    path_obj = pathlib.PureWindowsPath(file_path)
-    with open(path_obj.as_posix(), 'r', encoding='UTF-8') as fh:
+    with open(file_path, 'r', encoding='UTF-8') as fh:
         while True:
             data = fh.readline()
             if data == '':
                 break
-            sal_sum += int(data.split(',')[1])
+            sal_sum += int(data.split(',')[1]) # calculate salary sum
             employee_count += 1
     sal_avg = sal_sum/employee_count
-    salary_data = (sal_sum, sal_avg)
+    salary_data = (sal_sum, sal_avg) # write salary values into a tuple
     return salary_data
 
+
 salary_sum, salary_average = total_salary('D:/basic-projects/goit-algo-hw-04/monthly-salary.txt')
+
 print(f"Загальна сума заробітної плати: {salary_sum}, Середня заробітна плата: {salary_average}")
---
+++ pyCore_mod4_task2.py
@@ -0,0 +1,29 @@
+def get_cat_data(path=str()):
+    cat_data = []
+    try :
+        with open(path, "r", encoding="utf-8") as fh:
+            while True:
+                line_data = str(fh.readline())
+
+                if line_data == '':
+                    break
+
+                split_data = line_data.split(',') # [id, name, age]
+
+                for i in range(len(split_data)):
+                    split_data[i] = split_data[i].strip() # stripping spaces and line breaks
+
+                cat = {"id" : split_data[0], 
+                    "name" : split_data[1], 
+                    "age" : split_data[2]}
+                
+                cat_data.append(cat)
+
+    except FileNotFoundError:
+        return 'Error - file not found!'
+    else:
+        return cat_data
+
+cats_info = get_cat_data('D:/basic-projects/goit-algo-hw-04/goit-algo-hw-04/cat-data.txt')
+
+print(cats_info)
