a5a41ec31f36d50361ba2278935aa485a48f0449
Michael Dorokhov <dorohov1008@gmail.com>
2026-02-17 15:46:32 +0200

    Add task 4

---
+++ pyCore_mod4_task4.py
@@ -0,0 +1,32 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added"
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Goodbye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "all":
+            print(contacts)
+        else:
+            print("Invalid command")
+
+
+main()
